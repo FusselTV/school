4e5d20e290ae0c764629b3498f92c639d80c17b7
Christian <github@fussel.tv>
2021-09-02 11:31:53 +0200

    finished aufgabe_9

--- aufgabe_9.py
+++ aufgabe_9.py
@@ -1,6 +1,13 @@
 points = ["x", "y"]
 i = 0
+section = int(1)
 while i <= 1:
     points[i] = (input("Gib den " + points[i] + "-Wert ein: "))
     i += 1
-print(points)
+if "-" in points[0]:
+    section += 1
+if "-" in points[1]:
+    section += 1
+if int(points[0]) >= 0 and "-" in points[1]:
+    section = 4
+print("\nDer Punkt (" + str(points[0]) + "|" + str(points[1]) + ") ist in Abschnitt " + str(section) +"!")
