b29d4cd6de29329ecc0192b1a7aa0e974ed6ec06
Christian <github@fussel.tv>
2021-09-10 11:01:25 +0200

    added analog_clock.py

---
+++ analog_clock.py
@@ -0,0 +1,6 @@
+def computeHourHandAngle (hours, minutes):
+    back = 1 / 2 * (60 * hours + minutes)
+    return back
+def computeMinuteHandAngle (minutes):
+    back = 6 * minutes
+    return back
