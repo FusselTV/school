c7c75d112ccddb0d289df4d1d1a6e1cae9805791
fussel <github@fussel.tv>
2021-09-12 17:34:03 +0200

    improved garmentSize.py

--- garmentSize.py
+++ garmentSize.py
@@ -1,14 +1,14 @@
-bodysize = int(input())
-chestsize = int(input())
-def computeGarmentSizW(bodysize, chestsize):
-    back = chestsize / 2 - 6
-    if bodysize > 170:
-        back = back * 2
-    elif bodysize < 164:
-        back = back / 2
-    return back
-def computeGarmentSize(bodysize, chestsize):
-    back = chestsize / 2
-    return back
-garmentSize = computeGarmentSizW(bodysize, chestsize)
+bodysize = int(input("Gib die Größe ein: "))
+chestsize = int(input("Gib den Brustumfang an: "))
+gender = (input("Gib ein Geschlecht ein: "))
+def computeGarmentSiz(bodysize, chestsize, gender):
+    if gender == "w" or "weiblich":
+        back = chestsize / 2 - 6
+        if bodysize > 170:
+            back = back * 2
+        elif bodysize < 164:
+            back = back / 2
+        return back
+    return chestsize / 2
+garmentSize = computeGarmentSiz(bodysize, chestsize, gender)
 print(bodysize, "cm groß,", chestsize, "cm Brustumfang:", garmentSize)
